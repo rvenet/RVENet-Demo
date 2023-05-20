fix - bbox calculation

preprocessing.py
@@ -184,8 +184,8 @@ def get_preprocessed_frames(dicom_file_path, fps=None, hr=None, orientation="May
 
     for i in range(len(gray_frames)):
         masked_image = np.where(erosion_on_binary_msk, gray_frames[i], 0)
-        cropped_image = masked_image[int(bbox.min_point.x):int(bbox.max_point.x),
-                                     int(bbox.min_point.y):int(bbox.max_point.y)]
+        cropped_image = masked_image[np.min(binary_mask_coordinates[:,0]):np.max(binary_mask_coordinates[:,0]),
+                                     np.min(binary_mask_coordinates[:,1]):np.max(binary_mask_coordinates[:,1])]
         cropped_frames.append(cropped_image)
 
     # Sampling frames from each cardiac cycle